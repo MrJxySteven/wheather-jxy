26efca872286ae38b253956bb5527dd1b99ba5e2
MrJxySteven <mr_jxy_steven@163.com>
2020-05-03 16:40:46 +0800

    初步添加和风天气

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '和风天气',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
